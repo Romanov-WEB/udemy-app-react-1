479ead7eef94796cbb24990ef2db529e51baa533
Zhenya <asgyrdjons@gmail.com>
2021-12-21 22:48:03 +0300

    like clik

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,24 +7,26 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
-  const data = [
-    {name:'John C.', salary: 800, increase: false},
-    {name:'Alex M.', salary: 3000, increase: true},
-    {name:'Carl W.', salary: 5000, increase: false}
-  ];
-  return (
-    <div className="app">
-        <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
+    const data = [
+        {name: 'John C.', salary: 800, increase: false, id: 1},
+        {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+        {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+    ];
+
+    return (
+        <div className="app">
+            <AppInfo />
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+            
+            <EmployeesList data={data}/>
+            <EmployeesAddForm/>
         </div>
-        
-        <EmployeesList data ={data}/>
-        <EmployeesAddForm/>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -3,11 +3,14 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 import './employees-list.css';
 
 const EmployeesList = ({data}) => {
+
     const elements = data.map(item => {
+        const {id, ...itemProps} = item;
         return (
-            <EmployeesListItem key={item.name} {...item} />
+            <EmployeesListItem key={id} {...itemProps}/>
         )
     })
+
     return (
         <ul className="app-list list-group">
             {elements}
