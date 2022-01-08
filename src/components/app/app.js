60b73e25c587c6f585eab175a4db5c584aa1e5f4
Zhenya <asgyrdjons@gmail.com>
2022-01-08 04:17:43 +0300

    delet-add-posts

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,4 @@
+import { Component } from 'react/cjs/react.production.min';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,27 +7,47 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
 
-    const data = [
-        {name: 'John C.', salary: 800, increase: false, id: 1},
-        {name: 'Alex M.', salary: 3000, increase: true, id: 2},
-        {name: 'Carl W.', salary: 5000, increase: false, id: 3}
-    ];
+    state= {
+        data:[
+            {name: 'John C.', salary: 800, increase: false, id: 1},
+            {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+            {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+        ]
+    }
 
-    return (
-        <div className="app">
-            <AppInfo />
+    addList = (list) => {
+        this.setState(({data})=> {
+            return {
+                data: [list, ...data]
+            }
+        })
+    }
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    deleteList = (list) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item !== list)
+            }
+        })
+    }
+
+    render(){
+        return (
+            <div className="app">
+                <AppInfo />
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                
+                <EmployeesList data={this.state.data} deleteList={this.deleteList} />
+                <EmployeesAddForm addList={this.addList}/>
             </div>
-            
-            <EmployeesList data={data}/>
-            <EmployeesAddForm/>
-        </div>
-    );
+        )
+    }
 }
 
 export default App;
